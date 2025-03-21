c37a46d727258a8cbcf8938c7d66b5deaa7f7483
Vimala S <vimala.sellappan@stud.sundsgarden.se>
2025-03-21 05:48:38 +0100

    tried to show the discounted price in html page

--- Discount.js
+++ Discount.js
@@ -3,3 +3,4 @@ let price=input.slice(1);
 let new_price = parseFloat(price);
 let discounted_price = new_price- (new_price * 0.10);
 console.log('Your discounted price is: $' + discounted_price);
+document.body.innerHTML += `<p>Your discounted price is: $${discounted_price}</p>`;
